4e6c551340b5eb4460c89946415fd7d7f242a48e
Thanakorn-ki <thanakornaum11@gmail.com>
2024-06-17 00:28:30 +0700

    test

---
+++ nvims/.config/nvim/lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+local opt = vim.opt
+opt.sidescroll = 8
+opt.spell = true
+opt.scrolloff = 12
